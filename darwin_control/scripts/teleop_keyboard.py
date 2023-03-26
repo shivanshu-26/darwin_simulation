5086ccb81f8e1f6a07a1044d3166cbefeeae1c20
shivanshu-26 <120156096+shivanshu-26@users.noreply.github.com>
2023-03-26 20:13:07 +0530

    Update teleop_keyboard.py

--- darwin_control/scripts/teleop_keyboard.py
+++ darwin_control/scripts/teleop_keyboard.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from geometry_msgs.msg import Twist
@@ -17,7 +17,7 @@ ANG_VEL_STEP  = 0.1
 
 info = """
 ----------------------------------------
-Husky Robot Keyboard Teleoperation Panel
+Darwin Robot Keyboard Teleoperation Panel
 ----------------------------------------
                  W
              A   S   D
